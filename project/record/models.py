7ecbd8102fe25e51e654aefd119fab894d890ffe
Jivi1512 <jivansh1513@gmail.com>
2025-03-25 22:20:34 +0530

    minor change

--- project/record/models.py
+++ project/record/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Record(models.Model):
     fname=models.CharField(max_length=255)
     lname=models.CharField(max_length=255)
+    City=models.CharField(max_length=20,null=True)
+    Age=models.IntegerField(null=True)
